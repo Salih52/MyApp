faa49e6198c537b24dcb4b891f33d66a886778a8
Salih52 <saliheliack@gmail.com>
2025-07-30 17:26:25 +0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,27 +2,26 @@ import { AfterViewInit, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { slideInAnimation } from './route-animations';
 import {
-    TranslateService,
-    TranslatePipe,
-    TranslateDirective
-} from "@ngx-translate/core";
+  TranslateService,
+  TranslatePipe,
+  TranslateDirective,
+} from '@ngx-translate/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  animations: [slideInAnimation]
+  animations: [slideInAnimation],
 })
 export class AppComponent implements AfterViewInit {
-
   constructor(private translate: TranslateService) {
-        this.translate.addLangs(['tr', 'en']);
-        this.translate.setFallbackLang('en');
-        this.translate.use('en');
-    }
+    this.translate.addLangs(['tr', 'en']);
+    this.translate.setFallbackLang('en');
+    this.translate.use('en');
+  }
 
   ngAfterViewInit(): void {
     // Initialize AOS (Animate On Scroll) after the view has been initialized
-    import('aos').then(AOS => {
+    import('aos').then((AOS) => {
       AOS.init({
         duration: 1200, // Animation duration in milliseconds
         once: true, // Whether animation should happen only once while scrolling down
